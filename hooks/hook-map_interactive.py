2ddefa69d65141842f04762b9c96bccc0efe0fdf
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-07-11 20:47:56 -0400

    new hooks for pyinstaller

--- hooks/hook-map_interactive.py
+++ hooks/hook-map_interactive.py
@@ -8,3 +8,5 @@ fp = ei.get_curdir()
 datas = []
 for l in os.listdir(os.path.join(fp,'flt_module')):
     datas.append((os.path.join(fp,'flt_module',l),'flt_module'))
+for l in os.listdir(os.path.join(fp,'map_icons')):
+    datas.append((os.path.join(fp,'map_icons',l),'map_icons'))
